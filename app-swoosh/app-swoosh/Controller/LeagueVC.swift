cf10e92c9786410deca9880bafbf502da05efac4
DaiBoonz <ngocdaiute@gmail.com>
2022-06-27 15:11:17 +0700

    passing data between viewcontrollers

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -8,13 +8,17 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
-   
+    @IBOutlet weak var NextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       player = Player()
     }
+    
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "SkillVCSegue", sender: self)
@@ -22,9 +26,29 @@ class LeagueVC: UIViewController {
     @IBAction func unwindFromSkillVC1(unwindseuge1: UIStoryboardSegue){}
 
     
-
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectedLeauge(leagueType: "mens")
+    }
     
 
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectedLeauge(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectedLeauge(leagueType: "coed")
+    }
+    
+    func selectedLeauge(leagueType: String){
+        player.desiredLeague = leagueType
+        NextBtn.isEnabled = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC{
+            skillVC.player = player
+        }
+    }
     /*
     // MARK: - Navigation
 
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by MacOS-BigSur on 27/06/2022.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
