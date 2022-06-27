8d57bbf866fadcc4a056e58695aad694624e1fb5
DaiBoonz <ngocdaiute@gmail.com>
2022-06-27 10:50:40 +0700

    Refactoring

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,31 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by MacOS-BigSur on 27/06/2022.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func unwindFromSkillVC1(unwindseuge1: UIStoryboardSegue){}
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
